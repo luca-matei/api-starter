1ff0b7726d72a28efca1cee6da718f2a6dbf7a82
Luca Matei <dev@lucamatei.eu>
2024-04-11 15:40:48 +0300

    Add Redis

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import uvicorn
 
 from fastapi import FastAPI
 
+from v1.clients.redis import RedisSession
+
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
+    with RedisSession(db=0, decode="utf-8") as cache:
+        cache.set("key", "value")
+        print(cache.get("key"))
+
     return {"Hello": "World"}
 
 
